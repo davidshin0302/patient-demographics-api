c6d28bccf13b1d97d3c880d7b5937f1f47fc61b9
github username <github email address>
2025-01-06 15:41:25 -0500

    first commit

---
+++ src/main/java/com/abernathyclinic/patientdemographics/PatientDemographicsApiApplication.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.patientdemographics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientDemographicsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientDemographicsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abernathyclinic/patientdemographics/PatientDemographicsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abernathyclinic.patientdemographics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientDemographicsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
