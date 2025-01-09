5decdbc7efce34102565a02ae620b0625b4403f7
github username <github email address>
2025-01-09 16:09:35 -0500

    Patient table now use UNIQUE constraints.

--- src/main/java/com/abernathyclinic/patientdemographics/model/Patient.java
+++ src/main/java/com/abernathyclinic/patientdemographics/model/Patient.java
@@ -1,6 +1,7 @@
 package com.abernathyclinic.patientdemographics.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,23 +9,29 @@ import lombok.Data;
 @Data
 @Entity
 @Builder
-@Table(name = "patients")
+@Table(name = "patients", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "date_of_birth"), // dob must be unique
+        @UniqueConstraint(columnNames = {"given_name", "family_name"}), // combination of given and family name must be unique.
+})
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(name = "given_name", nullable = false)
     private String givenName;
 
-
+    @NotBlank
     @Column(name = "family_name", nullable = false)
     private String familyName;
 
+    @NotBlank
     @Column(name = "date_of_birth", nullable = false)
     private String dateOfBirth;
 
+    @NotBlank
     @Column(nullable = false)
     private String sex;
 
