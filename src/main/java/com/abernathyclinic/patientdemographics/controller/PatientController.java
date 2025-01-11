3fb6f6ec31c1b34c42746aa6f273bd9efd878596
davidshin0302 <david.shin0302@gmail.com>
2025-01-11 16:18:36 -0500

    implemented test edge cases for PatientController.

--- src/main/java/com/abernathyclinic/patientdemographics/controller/PatientController.java
+++ src/main/java/com/abernathyclinic/patientdemographics/controller/PatientController.java
@@ -9,7 +9,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -56,7 +55,7 @@ public class PatientController {
             log.error(ex.getMessage());
 
             responseEntity = ResponseEntity.status(HttpStatus.CONFLICT).build();
-        } catch (Exception ex) {
+        } catch (RuntimeException ex) {
             log.error("Unable to add new patent: {}", patient);
             log.error(ex.getMessage());
 
